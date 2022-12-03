7787986bfeb51361b2eab64778bc793aa74ecb3f
Adi Narayana Reddy <adinarayana4444@gmail.com>
2022-12-03 15:38:37 +0530

    Movie Booking App

---
+++ src/Routes/AppRoutes.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Login from '../pages/authentication/Login/Login'
+import Signup from '../pages/authentication/Signup/Signup'
+
+const AppRoutes = () => {
+   return (
+      <Router>
+         <Routes>
+            <Route path='/' element={<Login />} />
+            <Route path='/signup' element={<Signup />} />
+         </Routes>
+      </Router>
+   )
+}
+
+export default AppRoutes
---
+++ src/pages/authentication/Signup/Signup.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./signup.css";
+
+const Signup = () => {
+	return (
+		<div>
+			<h1>Signup</h1>
+		</div>
+	);
+};
+
+export default Signup;
